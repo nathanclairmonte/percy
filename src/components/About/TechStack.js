41567235a6a020094d2f5a52e579821bbb4db975
nathanclairmonte <nathanclairmonte@outlook.com>
2023-01-25 21:40:36 -0500

    commit with old tech stack method

--- src/components/About/TechStack.js
+++ src/components/About/TechStack.js
@@ -26,97 +26,97 @@ import {
     SiGooglecloud,
 } from "react-icons/si";
 
-const technologies = [
-    {
-        name: "Python",
-        icon: <DiPython className="tech-icon" />,
-    },
-    {
-        name: "Git",
-        icon: <DiGit className="tech-icon" />,
-    },
-    {
-        name: "Pandas",
-        icon: <SiPandas className="tech-icon" />,
-    },
-    {
-        name: "TensorFlow",
-        icon: <SiTensorflow className="tech-icon" />,
-    },
-    {
-        name: "PyTorch",
-        icon: <SiPytorch className="tech-icon" />,
-    },
-    {
-        name: "Keras",
-        icon: <SiKeras className="tech-icon" />,
-    },
-    {
-        name: "NumPy",
-        icon: <SiNumpy className="tech-icon" />,
-    },
-    {
-        name: "SciPy",
-        icon: <SiScipy className="tech-icon" />,
-    },
-    {
-        name: "SQL",
-        icon: <AiOutlineConsoleSql className="tech-icon" />,
-    },
-    {
-        name: "JavaScript",
-        icon: <SiJavascript className="tech-icon" />,
-    },
-    {
-        name: "TypeScript",
-        icon: <SiTypescript className="tech-icon" />,
-    },
-    {
-        name: "React",
-        icon: <FaReact className="tech-icon" />,
-    },
-    {
-        name: "React-Native",
-        icon: <FaReact className="tech-icon" />,
-    },
-    {
-        name: "Bootstrap",
-        icon: <SiBootstrap className="tech-icon" />,
-    },
-    {
-        name: "Node.js",
-        icon: <FaNode className="tech-icon" />,
-    },
-    {
-        name: "Flask",
-        icon: <SiFlask className="tech-icon" />,
-    },
-    {
-        name: ".NET Framework",
-        icon: <SiDotnet className="tech-icon" />,
-    },
-    {
-        name: "AWS",
-        icon: <FaAws className="tech-icon" />,
-    },
-    {
-        name: "Google Cloud Platform",
-        icon: <SiGooglecloud className="tech-icon" />,
-    },
-    {
-        name: "C#",
-        icon: <TbCSharp className="tech-icon" />,
-    },
-    {
-        name: "Docker",
-        icon: <FaDocker className="tech-icon" />,
-    },
-];
+// const technologies = [
+//     {
+//         name: "Python",
+//         icon: <DiPython className="tech-icon" />,
+//     },
+//     {
+//         name: "Git",
+//         icon: <DiGit className="tech-icon" />,
+//     },
+//     {
+//         name: "Pandas",
+//         icon: <SiPandas className="tech-icon" />,
+//     },
+//     {
+//         name: "TensorFlow",
+//         icon: <SiTensorflow className="tech-icon" />,
+//     },
+//     {
+//         name: "PyTorch",
+//         icon: <SiPytorch className="tech-icon" />,
+//     },
+//     {
+//         name: "Keras",
+//         icon: <SiKeras className="tech-icon" />,
+//     },
+//     {
+//         name: "NumPy",
+//         icon: <SiNumpy className="tech-icon" />,
+//     },
+//     {
+//         name: "SciPy",
+//         icon: <SiScipy className="tech-icon" />,
+//     },
+//     {
+//         name: "SQL",
+//         icon: <AiOutlineConsoleSql className="tech-icon" />,
+//     },
+//     {
+//         name: "JavaScript",
+//         icon: <SiJavascript className="tech-icon" />,
+//     },
+//     {
+//         name: "TypeScript",
+//         icon: <SiTypescript className="tech-icon" />,
+//     },
+//     {
+//         name: "React",
+//         icon: <FaReact className="tech-icon" />,
+//     },
+//     {
+//         name: "React-Native",
+//         icon: <FaReact className="tech-icon" />,
+//     },
+//     {
+//         name: "Bootstrap",
+//         icon: <SiBootstrap className="tech-icon" />,
+//     },
+//     {
+//         name: "Node.js",
+//         icon: <FaNode className="tech-icon" />,
+//     },
+//     {
+//         name: "Flask",
+//         icon: <SiFlask className="tech-icon" />,
+//     },
+//     {
+//         name: ".NET Framework",
+//         icon: <SiDotnet className="tech-icon" />,
+//     },
+//     {
+//         name: "AWS",
+//         icon: <FaAws className="tech-icon" />,
+//     },
+//     {
+//         name: "Google Cloud Platform",
+//         icon: <SiGooglecloud className="tech-icon" />,
+//     },
+//     {
+//         name: "C#",
+//         icon: <TbCSharp className="tech-icon" />,
+//     },
+//     {
+//         name: "Docker",
+//         icon: <FaDocker className="tech-icon" />,
+//     },
+// ];
 
 const TechStack = () => {
     return (
         <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-            {/* <Col xs={4} md={2} className="tech-icon-container">
+            <Col xs={4} md={2} className="tech-icon-container">
                 <DiPython className="tech-icon" /> Python
             </Col>
             <Col xs={4} md={2} className="tech-icon-container">
@@ -178,12 +178,12 @@ const TechStack = () => {
             </Col>
             <Col xs={4} md={2} className="tech-icon-container">
                 <FaDocker className="tech-icon" /> Docker
-            </Col> */}
-            {technologies.map((tech) => (
+            </Col>
+            {/* {technologies.map((tech) => (
                 <Col xs={4} md={2} className="tech-icon-container">
                     {tech.icon} {tech.name}
                 </Col>
-            ))}
+            ))} */}
         </Row>
     );
 };
