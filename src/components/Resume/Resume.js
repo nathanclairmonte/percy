c1e471e6b576c88351ce86bb7c2d28e4c5e3995a
nathanclairmonte <nathanclairmonte@outlook.com>
2023-01-26 19:40:06 -0500

    resume displays now but still some issues

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,9 +3,13 @@ import { Container, Row, Button } from "react-bootstrap";
 import Particle from "../Particle/Particle";
 import resume from "../../assets/Nathan-Clairmonte-CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page } from "react-pdf";
+import { Document, Page, pdfjs } from "react-pdf";
 // import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "./Resume.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+// const resumeLink =
+//     "https://raw.githubusercontent.com/nathanclairmonte/percy/main/src/assets/Nathan-Clairmonte-CV.pdf";
 
 const Resume = () => {
     const [width, setWidth] = useState(1200);
@@ -22,7 +26,7 @@ const Resume = () => {
                     variant="primary"
                     href={resume}
                     target="_blank"
-                    style={{ maxWidth: "250px" }}
+                    style={{ maxWidth: "250px", marginBottom: "20px" }}
                 >
                     <AiOutlineDownload /> &nbsp;Download CV
                 </Button>
@@ -30,8 +34,8 @@ const Resume = () => {
 
             <Row className="resume-document">
                 <Document file={resume} className="d-flex justify-content-center">
-                    <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-                    <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
+                    <Page pageNumber={1} scale={width > 786 ? 1.5 : 0.6} />
+                    {/* <Page pageNumber={2} scale={width > 786 ? 1.5 : 0.6} /> */}
                 </Document>
             </Row>
 
@@ -40,7 +44,7 @@ const Resume = () => {
                     variant="primary"
                     href={resume}
                     target="_blank"
-                    style={{ maxWidth: "250px" }}
+                    style={{ maxWidth: "250px", marginBottom: "20px" }}
                 >
                     <AiOutlineDownload /> &nbsp;Download CV
                 </Button>
