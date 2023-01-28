e269e84fcc469bd8efa2aab8690d66fca5724cf6
nathanclairmonte <nathanclairmonte@outlook.com>
2023-01-27 20:04:50 -0500

    resume displays! dl works too. not clickable tho

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,31 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container, Row, Button } from "react-bootstrap";
 import Particle from "../Particle/Particle";
-import resume from "../../assets/Nathan-Clairmonte-CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import { Card, CardMedia } from "@mui/material";
 import "./Resume.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-// const resumeLink =
-//     "https://raw.githubusercontent.com/nathanclairmonte/percy/main/src/assets/Nathan-Clairmonte-CV.pdf";
+const resumeDownload =
+    "https://drive.google.com/uc?export=download&id=1qpJv9xU2JQRM2CBkt9y0Zwjjyxck3Knf";
 
 const Resume = () => {
-    const [width, setWidth] = useState(1200);
-
-    useEffect(() => {
-        setWidth(window.innerWidth);
-    }, []);
-
     return (
         <Container fluid className="resume-section">
             <Particle />
             <Row style={{ justifyContent: "center", position: "relative" }}>
                 <Button
                     variant="primary"
-                    href={resume}
-                    target="_blank"
+                    href={resumeDownload}
                     style={{ maxWidth: "250px", marginBottom: "20px" }}
                 >
                     <AiOutlineDownload /> &nbsp;Download CV
@@ -33,17 +23,20 @@ const Resume = () => {
             </Row>
 
             <Row className="resume-document">
-                <Document file={resume} className="d-flex justify-content-center">
-                    <Page pageNumber={1} scale={width > 786 ? 1.5 : 0.6} />
-                    {/* <Page pageNumber={2} scale={width > 786 ? 1.5 : 0.6} /> */}
-                </Document>
+                <Card sx={{ maxWidth: 824 }}>
+                    <CardMedia
+                        className="cardmedia"
+                        component="iframe"
+                        height="1056px"
+                        src="https://drive.google.com/file/d/1qpJv9xU2JQRM2CBkt9y0Zwjjyxck3Knf/preview"
+                    />
+                </Card>
             </Row>
 
             <Row style={{ justifyContent: "center", position: "relative" }}>
                 <Button
                     variant="primary"
-                    href={resume}
-                    target="_blank"
+                    href={resumeDownload}
                     style={{ maxWidth: "250px", marginBottom: "20px" }}
                 >
                     <AiOutlineDownload /> &nbsp;Download CV
